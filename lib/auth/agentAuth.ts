5bb7bc56830f1b40f3a2a1a355f3daadac26b446
lim0n3 <simonestaffa96@gmail.com>
2025-06-04 18:19:27 +0200

    fix agent secret

--- lib/auth/agentAuth.ts
+++ lib/auth/agentAuth.ts
@@ -1,7 +1,7 @@
 import { env } from "../env";
 
 export function checkAgentSecret(req: Request) {
-  const headerSecret = req.headers.get("authorization");
+  const headerSecret = req.headers.get("x-agent-secret");
   const agentSecret = env.AGENT_SECRET;
   return headerSecret && agentSecret && headerSecret === agentSecret;
 }
