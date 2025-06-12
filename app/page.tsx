b87e6be15b419815ea2ce8685dbab69cf62e7478
lim0n3 <simonestaffa96@gmail.com>
2025-06-12 18:14:51 +0200

    fix frame url

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata({
       action: {
         type: "launch_frame",
         name: "Squabble",
-        url: `${appUrl}/games/${requestId}`,
+        url: appUrl,
         splashImageUrl: `${appUrl}/images/icon.png`,
         splashBackgroundColor: "#1B7A6E",
       },
